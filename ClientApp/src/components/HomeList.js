5ea256fb58b88a0dec2368e45d6ece48e7db8ca1
williammabotjadev <williammabotjadev@gmail.com>
2023-02-23 11:44:55 +0200

    single article component

--- ClientApp/src/components/HomeList.js
+++ ClientApp/src/components/HomeList.js
@@ -3,23 +3,22 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
+import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 
 import axios from 'axios';
 
 export default function HomeList() {
 
-  const [articles, setArticles] = React.useState(null);
+  const [articles, setArticles] = React.useState([]);
 
   function getArticles()
   {
-      const url = "https://localhost:44446";
+      const url = "https://localhost:44446/api/articles";
       axios.get(url)
       .then(res => {
-        console.log(res);
-        setArticles(state => res);
+        console.log(res.data);
+        setArticles(state => res.data);
       })
       .catch(err => console.log(err));
   }
@@ -33,23 +32,21 @@ export default function HomeList() {
       {articles?.map((article, index) => 
        <>
         <List sx={{ width: '100%', maxWidth: 640, bgcolor: 'background.paper' }}>
-          <ListItem alignItems="flex-start">
+          <ListItem key={index} alignItems="flex-start">
+            <Link to={`https://localhost:44446/api/articles/${article.id}`}>
             <ListItemText
-              primary="Brunch this weekend?"
+              primary={article.title}
               secondary={
                 <React.Fragment>
-                  <Typography
-                    sx={{ display: 'inline' }}
-                    component="span"
-                    variant="body2"
-                    color="text.primary"
-                  >
-                    {article.title}
-                  </Typography>
-                  {article.body.substring(0, 30)}
+                  {article.body.substring(0, 50)}...
                 </React.Fragment>
               }
             />
+            </Link>
+            <br />
+            <Typography variant="body2">
+              <strong>Author : {article.author}</strong>
+            </Typography>
           </ListItem>
           <Divider variant="inset" component="li" />
          
---
+++ ClientApp/src/components/SingleArticle.js
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { experimentalStyled as styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function SingleArticle() {
+
+  const [article, setArticle] = React.useState(null);
+
+  function getArticle()
+  {
+    const id = 23;
+    const url = `https://localhost:44446/api/articles/${id}`;
+
+    axios.get(url)
+    .then(res => {
+        console.log(res.data);
+        setArticle(res.data);
+    })
+  }
+
+  React.useEffect(() => {
+        getArticle();
+  }, [])
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+            <br />
+            <Typography variant="h3">
+                {article.title}
+            </Typography>
+            <br />
+            <Typography variant="body1">
+                {article.body}
+            </Typography>
+            <br />
+            <Typography variant="body2">
+                {article.author}
+            </Typography>
+      </Grid>
+    </Box>
+  );
+}
